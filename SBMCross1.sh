6028e9b8424de7a68e344aabd5fd2a2c8b886a0e
Poirier Lab <cpoirierlab@gmail.com>
2025-05-07 20:11:59 +0100

    Update SBMCross1.sh

--- SBMCross1.sh
+++ SBMCross1.sh
@@ -2,8 +2,8 @@
 
 subject=$1
 
-cd /home/nathan/Documents/RescanFull/Reconstructions
+cd /home/Documents/MRI/Reconstructions
 export SUBJECTS_DIR=$PWD
 
-recon-all -i /home/nathan/Documents/RescanFull/${subject}/T1MaskBias.nii.gz -s ${subject} -motioncor -talairach -hires
+recon-all -i /home/Documents/MRI/${subject}/T1MaskBias.nii.gz -s ${subject} -motioncor -talairach -hires
 recon-all -s ${subject} -talairach -use-mritotal -clean-tal # alternative talairach transform
