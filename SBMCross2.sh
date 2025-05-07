ecd3732c0118572b2fcf5b4e3036a3c8c4a60f9a
Poirier Lab <cpoirierlab@gmail.com>
2025-05-07 20:13:11 +0100

    Update SBMCross2.sh

--- SBMCross2.sh
+++ SBMCross2.sh
@@ -2,15 +2,15 @@
 
 subject=$1
 
-cd /home/nathan/Documents/RescanFull/Reconstructions
+cd /home/Documents/MRI/Reconstructions
 export SUBJECTS_DIR=$PWD
 
-cp /home/nathan/Documents/ManualEdits/${subject}/talairach.xfm ${subject}/mri/transforms/talairach.xfm
+cp /home/Documents/ManualEdits/${subject}/talairach.xfm ${subject}/mri/transforms/talairach.xfm
 
 recon-all -s ${subject} -nuintensitycor -normalization
-cp ${subject}/mri/T1.mgz ${subject}/mri/brainmask.mgz # brain is already skullstripped, so we can just copy T1.mgz to brainmask.mgz
+cp ${subject}/mri/T1.mgz ${subject}/mri/brainmask.mgz # brain is already skullstripped, so just copy T1.mgz to brainmask.mgz
 
-# # # # # use the corrected talairach.xfm as initialization for the subcortical registration (talairach.lta)
+# # # # # use corrected talairach.xfm as initialization for the subcortical registration (talairach.lta)
 lta_convert --ltavox2vox --inmni ${subject}/mri/transforms/talairach.xfm \
 --outlta ${subject}/mri/transforms/talairach.lta \
 --src ${subject}/mri/T1.mgz --trg $FREESURFER_HOME/average/RB_all_2016-05-10.vc700.gca
